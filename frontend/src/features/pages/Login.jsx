e6b7bd090d5c01fee6aff7d6474323b47e2338e0
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-29 21:38:33 +0500

    newcode

--- frontend/src/features/pages/Login.jsx
+++ frontend/src/features/pages/Login.jsx
@@ -10,15 +10,17 @@ const Login = () => {
 
           <form action="">
 
-          <div className='input-group'>
-            <label htmlFor="email">Email</label>
-            <input type="email" id='email' name='email' placeholder='Enter your Email' />
-           </div>
-           
-          <div className='input-group'>
-            <label htmlFor="password">Email</label>
-            <input type="password" id='password' name='password' placeholder='Enter your Password' />
-           </div>
+            <div className='input-group'>
+              <label htmlFor="email">Email</label>
+              <input type="email" id='email' name='email' placeholder='Enter your Email' />
+            </div>
+
+            <div className='input-group'>
+              <label htmlFor="password">Password</label>
+              <input type="password" id='password' name='password' placeholder='Enter your Password' />
+            </div>
+
+            <button className='button-primary'></button>
 
           </form>
         </div>
