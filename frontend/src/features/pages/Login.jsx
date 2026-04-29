c9250bd9fa447966dfaf53595a1506eb0e5b90ed
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-29 20:02:27 +0500

    newcode

--- frontend/src/features/pages/Login.jsx
+++ frontend/src/features/pages/Login.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <main>
+        <div className="form-container">
+
+          <h1>Login From</h1>
+
+          <form action="">
+
+          <div className='input-group'>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' name='email' placeholder='Enter your Email' />
+           </div>
+           
+          <div className='input-group'>
+            <label htmlFor="password">Email</label>
+            <input type="password" id='password' name='password' placeholder='Enter your Password' />
+           </div>
+
+          </form>
+        </div>
+      </main>
+    </>
   )
 }
 
