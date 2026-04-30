6cac0f9ab5387ab3043d3d628a69555509a02f67
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-30 17:14:06 +0500

    newcode

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,10 +1,37 @@
 import axios from 'axios'
 
-async function registerUser(username, email, password) {
+async function register(username, email, password) {
   try {
     const response = await axios.post(
       "http://localhost:3200/api/auth/register",
       { username, email, password },   // body (data)
+      { withCredentials: true }        // config
+    )
+
+    console.log(response.data)
+  } catch (error) {
+    console.log(error.response?.data || error.message)
+  }
+}
+
+async function login( email, password) {
+  try {
+    const response = await axios.post(
+      "http://localhost:3200/api/auth/login",
+      {  email, password },   // body (data)
+      { withCredentials: true }        // config
+    )
+
+    console.log(response.data)
+  } catch (error) {
+    console.log(error.response?.data || error.message)
+  }
+}
+
+async function logout() {
+  try {
+    const response = await axios.post(
+      "http://localhost:3200/api/auth/login", 
       { withCredentials: true }        // config
     )
 
